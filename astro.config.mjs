33f1e04ba484116b728b543183311ed4fbb03692
Rogério Matsumoto Figueiredo <rogerio@Rogerios-MacBook-Pro-2.local>
2026-07-24 18:45:52 +0200

    Expose Airtable env vars to build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,10 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  vite: {
+    define: {
+      'import.meta.env.AIRTABLE_BASE_ID': JSON.stringify(process.env.AIRTABLE_BASE_ID),
+      'import.meta.env.AIRTABLE_API_KEY': JSON.stringify(process.env.AIRTABLE_API_KEY),
+    },
+  },
 });
